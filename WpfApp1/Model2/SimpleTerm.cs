e7eab9e17de65569adf13a111b9d17d64eb57f6e
anastasiakova <kovalkov@post.bgu.ac.il>
2018-11-26 18:14:03 +0200

    fix saar made a bubu fix hyphen terms

--- WpfApp1/Model2/SimpleTerm.cs
+++ WpfApp1/Model2/SimpleTerm.cs
@@ -11,17 +11,20 @@ namespace Model2
         private string _term;
         private bool _isLowerCase;
         private string _postingPath;
+        private int _df;
 
         public SimpleTerm(string term, string path, bool isLower)
         {
             this._term = term;
             this._isLowerCase = isLower;
             this._postingPath = path;
+            this.Df = 1;
         }
     
         public string GetTerm { get => _term; set => _term = value; }
         public string PostingPath { get => _postingPath; }
         public bool IsLowerCase { get => _isLowerCase; set => _isLowerCase = value; }
+        public int Df { get => _df; set => _df = value; }
 
         override
         public int GetHashCode()
