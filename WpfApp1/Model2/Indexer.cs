48e3ffccf7bf3b7973619647523bb4d5e50bb73e
Saar <saarzeev@post.bgu.ac.il>
2018-11-27 18:34:29 +0200

    Methods stolen from Indexer into PostingSets

--- WpfApp1/Model2/Indexer.cs
+++ WpfApp1/Model2/Indexer.cs
@@ -20,93 +20,35 @@ namespace Model2
         {
             this._initialPathForPosting = path;
         }
-        
-        public Queue<Posting> setDocVocabularytoFullVocabulary(Doc doc ,SortedDictionary<string, Term> docDictionary)
+
+        public Queue<Posting> setDocVocabularytoFullVocabulary(Doc doc, SortedDictionary<string, Term> docDictionary)
         {
             string[] docPath = doc._path.Split('\\');
             Queue<Posting> docsPosting = new Queue<Posting>();
             foreach (string key in docDictionary.Keys)
             {
-                    if (fullDictionary.ContainsKey(key))
+                if (fullDictionary.ContainsKey(key))
+                {
+                    string posting = fullDictionary[key].PostingPath;
+                    if (docDictionary[key].IsLowerCase && !fullDictionary[key].IsLowerCase)
                     {
-                        string posting = fullDictionary[key].PostingPath;
-                        if (docDictionary[key].IsLowerCase && !fullDictionary[key].IsLowerCase)
-                        {
-                            fullDictionary[key].IsLowerCase = true ;
-                        }
-                        fullDictionary[key].Df++;
+                        fullDictionary[key].IsLowerCase = true;
                     }
-                    else
-                    {
-                        SimpleTerm newTerm = new SimpleTerm(key, "", docDictionary[key].IsLowerCase);
-                        fullDictionary.TryAdd(key, newTerm);
-                    }
+                    fullDictionary[key].Df++;
+                }
+                else
+                {
+                    SimpleTerm newTerm = new SimpleTerm(key, "", docDictionary[key].IsLowerCase);
+                    fullDictionary.TryAdd(key, newTerm);
+                }
                 docsPosting.Enqueue(new Posting(docPath[docPath.Length - 1], doc._indexInFile, docDictionary[key]));
             }
             return docsPosting;
         }
 
-        private void writePosting(string postingString, string path)
-        {
-            string PostPath = _initialPathForPosting + "\\" + path + ".txt";
-            dictionaryMutex.WaitOne();
-            using (var file = File.Open(PostPath, FileMode.Append))
-            {
-                using (BinaryWriter writer = new BinaryWriter(file))
-                {
-                    var bytes = Zip(postingString);
-                    writer.Write(bytes);
-                    writer.Close();
-                    file.Close();
-                }
-            }
-            dictionaryMutex.ReleaseMutex();
-        }
 
-        public static byte[] Zip(string str)
-        {
-            var bytes = Encoding.UTF8.GetBytes(str);
-
-            using (var msi = new MemoryStream(bytes))
-            using (var mso = new MemoryStream())
-            {
-                using (var gs = new GZipStream(mso, CompressionMode.Compress))
-                {
-                    msi.CopyTo(gs);
-                }
-                return mso.ToArray();
-            }
-        }
-
-        public static string Unzip(byte[] bytes)
-        {
-            using (var msi = new MemoryStream(bytes))
-            using (var mso = new MemoryStream())
-            {
-                using (var gs = new GZipStream(msi, CompressionMode.Decompress))
-                {
-                    gs.CopyTo(mso);
-                }
-                return Encoding.UTF8.GetString(mso.ToArray());
-            }
-        }
 
         // docPath(last value of path)+docId(int)+tf(int)+is100(0-false 1-true)+gapPositins(int[])+isLowerCase(0-false 1-true)
-       
-
-    public void mergeFiles()
-        {
-            var fileArray = Directory.EnumerateFiles(this._initialPathForPosting, "*.txt");
-            Queue<string> files = new Queue<string>(fileArray);
-            while (files.Count > 1)
-            {
-                var bytes = File.ReadAllBytes(files.Dequeue());
-                string unziped = Unzip(bytes);
-                
-            }
-            
-
-        }
-
     }
+
 } 
--- WpfApp1/Model2/PostingSets.cs
+++ WpfApp1/Model2/PostingSets.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -53,6 +55,75 @@ namespace Model2
             {
                 return capacity > 0;
             }
+
+            public void dumpToDisk()
+            {
+                foreach(string term in _termsDictionary.Keys)
+                {
+                    List<Posting> list = _termsDictionary[term];
+                    list.OrderByDescending(posting => posting.tf);
+
+                }
+            }
+
+            private void writePosting(string postingString)
+            {
+                string PostPath = _path + "\\" + path + ".txt";
+                using (var file = File.Open(PostPath, FileMode.Append))
+                {
+                    using (BinaryWriter writer = new BinaryWriter(file))
+                    {
+                        var bytes = Zip(postingString);
+                        writer.Write(bytes);
+                        writer.Close();
+                        file.Close();
+                    }
+                }
+            }
+
+            public static byte[] Zip(string str)
+            {
+                var bytes = Encoding.UTF8.GetBytes(str);
+
+                using (var msi = new MemoryStream(bytes))
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                    {
+                        msi.CopyTo(gs);
+                    }
+                    return mso.ToArray();
+                }
+            }
+
+            public static string Unzip(byte[] bytes)
+            {
+                using (var msi = new MemoryStream(bytes))
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                    {
+                        gs.CopyTo(mso);
+                    }
+                    return Encoding.UTF8.GetString(mso.ToArray());
+                }
+            }
+
+            public void mergeFiles()
+            {
+                /*var fileArray = Directory.EnumerateFiles(this._initialPathForPosting, "*.txt");
+                Queue<string> files = new Queue<string>(fileArray);
+                while (files.Count > 1)
+                {
+                    var bytes = File.ReadAllBytes(files.Dequeue());
+                    string unziped = Unzip(bytes);
+
+                }*/
+
+
+            }
+
         }
+    }
     }
 }
