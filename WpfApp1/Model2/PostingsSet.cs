3b60776a24e78d51f70ad3994c92a737107a08d2
Saar <saarzeev@post.bgu.ac.il>
2018-11-27 17:45:13 +0200

    Added PostingSet class

---
+++ WpfApp1/Model2/PostingsSet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Model2
+{
+    class PostingsSet
+    {
+        private static int counter = 0;
+        private Mutex mutex = new Mutex();
+        private Dictionary<string, List<StringBuilder>> _termsDictionary = new Dictionary<string, List<StringBuilder>>(); //terms, postings
+        private int id;
+        private static PostingsSet ps;
+
+
+
+        public PostingsSet()
+        {
+            id = counter;
+            counter++;
+        }
+        
+        public void Add(string term, StringBuilder posting)
+        {
+            mutex.WaitOne();
+            if (_termsDictionary.ContainsKey(term))
+            {
+                List<>
+            }
+            mutex.ReleaseMutex();
+        }
+    }
+}
