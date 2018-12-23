ee6a7b77944c5290a0fc50fc061f8202d24f4528
anastasiakova <kovalkov@post.bgu.ac.il>
2018-12-23 12:51:43 +0200

    query is only title

--- WpfApp1/Model2/Query.cs
+++ WpfApp1/Model2/Query.cs
@@ -14,19 +14,21 @@ namespace Model2
         private bool isStemming;
         private bool withSemantic;
         private Dictionary<int, StringBuilder> queries;
+        private Dictionary<int, StringBuilder> simToQuery;
 
-       public Query(HashSet<string> cities, bool isStemming, bool withSemantic)
+        public Query(HashSet<string> cities, bool isStemming, bool withSemantic)
         {
             this.cities = cities;
             this.isStemming = isStemming;
             this.withSemantic = withSemantic;
             queries = new Dictionary<int, StringBuilder>();
         }
-  
+
         public bool IsStemming { get => isStemming; set => isStemming = value; }
         public bool WithSemantic { get => withSemantic; set => withSemantic = value; }
         public HashSet<string> Cities { get => cities; set => cities = value; }
         public Dictionary<int, StringBuilder> Queries { get => queries; set => queries = value; }
+        public Dictionary<int, StringBuilder> SimToQuery { get => simToQuery; set => simToQuery = value; }
 
         public void runQueriesFromPath(string path)
         {
@@ -42,10 +44,10 @@ namespace Model2
                         {
                             int queryID = 0;
                             bool Query = false;
-                            bool narrative = false;
+                            bool queryEnd = false;
                             while ((line = streamReader.ReadLine()) != null && line != "</top>")
                             {
-                                if (!narrative && line.Length > 1)
+                                if (!queryEnd && line.Length > 1)
                                 {
                                     if (!Query)
                                     {
@@ -55,7 +57,7 @@ namespace Model2
                                             string[] splited = line.Split(':');
                                             if (splited.Length > 0)
                                             {
-                                                int.TryParse(splited.Last().Trim(),out queryID) ;
+                                                int.TryParse(splited.Last().Trim(), out queryID);
                                                 Query = true;
                                             }
                                         }
@@ -63,21 +65,18 @@ namespace Model2
                                     else
                                     {
                                         //query
-                                        if (!line.StartsWith("<title>"))
+                                        if (line.StartsWith("<title>"))
                                         {
                                             //TODO maybe read titel
                                             //TODO maybe parse query if question or regular text
                                             //TODO mayby try read narr
-                                            if (!line.StartsWith("<"))
+                                            string[] del = { "<title>"," "};
+                                            string[] splited = line.Split(del, StringSplitOptions.RemoveEmptyEntries);
+                                            if (splited.Length > 0)
                                             {
-                                                query.AppendFormat("{0}{1}", line, " ");
+                                                query.Append(String.Join(" ",splited));
+                                                queryEnd = true;
                                             }
-                                               
-                                        }
-                                        //narative for now erelevant
-                                        else
-                                        {
-                                            narrative = true;
                                         }
                                     }
                                 }
@@ -92,11 +91,19 @@ namespace Model2
                     }
                 }
             }
+            if (this.withSemantic)
+            {
+                addSemantic();
+            }
         }
 
         public void runSingleQuery(string query)
         {
             this.queries.Add(randomID, new StringBuilder(query));
+            if (this.withSemantic)
+            {
+                addSemantic();
+            }
             randomID++;
         }
         public void addSemantic()
