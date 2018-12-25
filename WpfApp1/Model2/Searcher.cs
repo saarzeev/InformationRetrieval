9c04a8e41614dc49fa6761e1585369b7580a9ef1
Saar <saarzeev@post.bgu.ac.il>
2018-12-25 13:53:16 +0200

    partial searcher

--- WpfApp1/Model2/Searcher.cs
+++ WpfApp1/Model2/Searcher.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Model2
 {
-    public class Searcher
+    public partial class Searcher
     {
         private string path;
         private bool isStemming;
@@ -38,40 +38,7 @@ namespace Model2
             return parsed;
         }
 
-        /// <summary>
-        /// Given a List of strings, <paramref name="term"/>, returns the terms' posting StringBuilder
-        /// </summary>
-        /// <param name="terms"></param>
-        /// <returns></returns>
-        public List<string> GetTermsPosting(List<string> terms)
-        {
-            List<string> ans = new List<string>();
-
-            for (int i = 0; i < terms.Count; i++)
-            {
-                string term = terms[i].ToLower();
-                char firstChar = term[0];
-                string firstLetter = "\\" + (term.ElementAt(0) >= 'a' && term.ElementAt(0) <= 'z' ? term.ElementAt(0).ToString() : "other");
-                string postingPath = Indexer.indexer.postingPathForSearch + ("\\" + firstLetter + "FINAL.txt");
-
-                //TODO Maybe stay in the same file for every term within that file. (Sort query terms first)
-
-                using (FileStream infile = new FileStream(postingPath, FileMode.Open, FileAccess.Read))
-                {
-                    if (Indexer.fullDictionary.ContainsKey(term))
-                    {
-                        long position = Indexer.fullDictionary[term].Position;
-                        using (StreamReader file = new StreamReader(infile))
-                        {
-                            infile.Seek(position, SeekOrigin.Begin);
-                            ans.Add(file.ReadLine());
-                        }
-                    }
-                }
-            }
-            //Console.WriteLine("Search duration: " + (DateTime.Now - startingTime));
-            return ans;
-        }
+        W
 
         public Dictionary<int, List<Tuple<string, double>>> initSearch(Indexer indexer)
         {
